16d4a7489d6d5b1fbeac7fb25f4df67990597152
TynK-M <164543445+TynK-M@users.noreply.github.com>
2025-04-07 14:32:16 +0200

    First commit

---
+++ src/main/java/org/matteopauroso/managerook/ManagerookApplication.java
@@ -0,0 +1,13 @@
+package org.matteopauroso.managerook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/matteopauroso/managerook/ManagerookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matteopauroso.managerook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
